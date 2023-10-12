bb8bb24c064379369e2740b94834c76ddbe03d14
cylinders <ogvruth@gmail.com>
2023-10-12 14:19:23 -0400

    This is a commit that adds my WEBSOCKET aws test feature

--- client.py
+++ client.py
@@ -3,12 +3,30 @@ import threading
 host = "192.168.0.64"
 port = 5555
 
+
+class client(): 
+    
+    # problems is a messed up way to word it, but consider it a STRING list of all of the problems they have 
+    # preference is the number of people that they want in their thing uyk w i mean
+    def __init__(name, address, password, problems, preference):
+        self.name = name
+        self.address = address
+        self.password = password
+        self.problemms = problems
+        self.preference = preference 
+        
+
+
+
+
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
 server.listen()
 
 clients = []
-nicknames = []
+
 
 def broadcast(message): 
     for client in clients: 
@@ -52,6 +70,8 @@ def recieve():
 
 #man, i am adding this comment literally just to test if git works and thats it lmao. 
 
+# TODO: 
+# create text file or sql database shit to add SOMETHING 
 recieve()
 
         
