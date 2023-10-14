f3551f59a84fea207a723adc7618b4512ef43043
cylinders <ogvruth@gmail.com>
2023-10-14 18:13:02 -0400

    added in chat gpt code

--- backendTEST.py
+++ backendTEST.py
@@ -1,106 +1,28 @@
-# Python program to implement server side of chat room. 
-import socket 
-import select 
-import sys 
-'''Replace "thread" with "_thread" for python 3'''
-from _thread import *
+import socket
 
-"""The first argument AF_INET is the address domain of the 
-socket. This is used when we have an Internet Domain with 
-any two hosts The second argument is the type of socket. 
-SOCK_STREAM means that data or characters are read in 
-a continuous flow."""
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+def start_server():
+    server_ip = "0.0.0.0"  # Listen on all available interfaces
+    server_port = 12345    # Choose a port (make sure it's not used by other services)
 
-# checks whether sufficient arguments have been provided 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(1)
 
-# takes the first argument from command prompt as IP address 
-IP_address = str("0.0.0.0") 
+    print(f"Server is listening on {server_ip}:{server_port}")
 
-# takes second argument from command prompt as port number 
-Port = int(12348) 
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection established with {client_address}")
 
-""" 
-binds the server to an entered IP address and at the 
-specified port number. 
-The client must be aware of these parameters 
-"""
-server.bind((IP_address, Port)) 
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        if not message:
+            break
+        print(f"Client: {message}")
+        response = input("You: ")
+        client_socket.send(response.encode('utf-8'))
 
-""" 
-listens for 100 active connections. This number can be 
-increased as per convenience. 
-"""
-server.listen(100) 
+    client_socket.close()
 
-list_of_clients = [] 
+if __name__ == "__main__":
+    start_server()
 
-def clientthread(conn, addr): 
-
-	# sends a message to the client whose user object is conn 
-	conn.send("Welcome to this chatroom!".encode('ascii')) 
-
-	while True: 
-			try: 
-				message = conn.recv(2048) 
-				if message: 
-
-					"""prints the message and address of the 
-					user who just sent the message on the server 
-					terminal"""
-					print ("<" + addr[0] + "> " + message) 
-
-					# Calls broadcast function to send message to all 
-					message_to_send = "<" + addr[0] + "> " + message 
-					broadcast(message_to_send, conn) 
-
-				else: 
-					"""message may have no content if the connection 
-					is broken, in this case we remove the connection"""
-					remove(conn) 
-
-			except: 
-				continue
-
-"""Using the below function, we broadcast the message to all 
-clients who's object is not the same as the one sending 
-the message """
-def broadcast(message, connection): 
-	for clients in list_of_clients: 
-			try: 
-				clients.send(message) 
-			except: 
-				clients.close() 
-
-				# if the link is broken, we remove the client 
-				remove(clients) 
-
-"""The following function simply removes the object 
-from the list that was created at the beginning of 
-the program"""
-def remove(connection): 
-	if connection in list_of_clients: 
-		list_of_clients.remove(connection) 
-
-while True: 
-
-	"""Accepts a connection request and stores two parameters, 
-	conn which is a socket object for that user, and addr 
-	which contains the IP address of the client that just 
-	connected"""
-	conn, addr = server.accept() 
-
-	"""Maintains a list of clients for ease of broadcasting 
-	a message to all available people in the chatroom"""
-	list_of_clients.append(conn) 
-
-	# prints the address of the user that just connected 
-	print (addr[0] + " connected")
-
-	# creates and individual thread for every user 
-	# that connects 
-	start_new_thread(clientthread,(conn,addr))	 
-
-conn.close() 
-server.close() 
--- frontendTEST.py
+++ frontendTEST.py
@@ -1,31 +1,22 @@
-# Python program to implement client side of chat room. 
-import socket 
-import select 
-import sys 
-import time
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-IP_address = str("69.118.154.76") 
-Port = int(8081) 
-server.connect((IP_address, Port)) 
+import socket
 
-while True: 
+def start_client():
+    server_ip = "SERVER_PUBLIC_IP"  # Replace with the public IP of your server
+    server_port = 12345             # Make sure it's the same port as in the server
 
-	# maintains a list of possible input streams 
-	sockets_list = [sys.stdin, server] 
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
 
-	""" There are two possible input situations. Either the 
-	user wants to give manual input to send to other people, 
-	or the server is sending a message to be printed on the 
-	screen. Select returns from sockets_list, the stream that 
-	is reader for input. So for example, if the server wants 
-	to send a message, then the if condition will hold true 
-	below.If the user wants to send a message, the else 
-	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
-    time.sleep(1) 
-    server.send("HELLO".encode("ascii"))
+    while True:
+        message = input("You: ")
+        client_socket.send(message.encode('utf-8'))
+        if message == "exit":
+            break
+        response = client_socket.recv(1024).decode('utf-8')
+        print(f"Server: {response}")
 
+    client_socket.close()
 
-server.close() 
-
+if __name__ == "__main__":
+    start_client()
 
