039d30e2c169873d59468ce43a55c6c605cf06ac
cylinders <ogvruth@gmail.com>
2023-10-26 13:42:11 -0400

    Restart once again

---
+++ backendClientMain.py
@@ -0,0 +1,12 @@
+import websockets, json
+import asyncio
+
+
+
+async def connectMainTest(websocketUrl): 
+
+    # Client Side Connect Test Client. 
+
+asyncio.run(connectMainTest(input("Enter Url: ")))
+
+
---
+++ vars.py
@@ -0,0 +1,8 @@
+def wssConnectUrl(): 
+    return "wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/single_user"
+
+def wssGcUrl(): 
+    return "wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/gc_pull"
+
+
+
