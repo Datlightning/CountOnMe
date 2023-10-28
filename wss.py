5bad7b327d196dfd18b29bd3cd71318d0d6ba911
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2023-10-28 19:26:04 -0400

    Changes to wss.py

--- wss.py
+++ wss.py
@@ -1,3 +1,7 @@
+# Still two different functions because it honestly doesn't make a difference and to make sure that some older code
+# will still work. 
+
+
 def wssConnectUrl(): 
     return "wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/gc_all/"
 
