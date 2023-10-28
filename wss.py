3eaea29cef2de3453d6efb4e889de890bd6aab30
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2023-10-28 19:25:52 -0400

    Just some small changes to wss.py

--- wss.py
+++ wss.py
@@ -1,5 +1,5 @@
 def wssConnectUrl(): 
-    return "wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/single_user"
+    return "wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/gc_all/"
 
 def wssGcUrl(): 
     return "wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/gc_all"
