df71ed7df6af0339fb38c7552010738d58181451
Shyiu <mightydragon334@gmail.com>
2023-10-28 19:24:09 -0400

    Switched to tkinter

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import (Color, Ellipse)
+# import customtkinter
+#
+# customtkinter.set_appearance_mode("light")
+# customtkinter.set_default_color_theme("dark-blue")
+#
+# root = customtkinter.CTk()
+# root.geometry("500x350")
+#
+# def login():
+#     print("Test")
+#
+#
+# frame = customtkinter.CTkFrame(master=root)
+# frame.pack(pady=20,padx=60, fill="both", expand=True)
+#
+# label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
+# label.pack(pady=12, padx=10)
+#
+# entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+# entry1.pack(pady=12, padx=10)
+#
+# entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show = "*")
+# entry2.pack(pady=12, padx=10)
+#
+# button = customtkinter.CTkButton(frame, text="Login", command=login)
+# button.pack(pady = 12, padx = 10)
+#
+# checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+# checkbox.pack(pady=12, padx = 10)
+#
+# root.mainloop()
+import customtkinter
 
-class MyPaintWidget(Widget):
-    def on_touch_move(self, touch):
-        with self.canvas:
-            Color(1, 1, 0)
-            d = 30.
-            Ellipse(pos=(touch.x - d/ 2, touch.y - d / 2), size=(d, d))
-class PaintApp(App):
-    def build(self):
-        return MyPaintWidget()
+customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-if __name__ == '__main__':
-    PaintApp().run()
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("400x240")
+
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+app.mainloop()
