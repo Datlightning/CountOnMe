c22203a3a30d202aa338f343b979a867f0eb6d63
Shyiu <mightydragon334@gmail.com>
2023-10-31 20:24:19 -0400

    Started css styling

--- src/app/services/websocket-service.ts
+++ src/app/services/websocket-service.ts
@@ -4,7 +4,7 @@ import { webSocket } from "rxjs/webSocket";
 @Injectable()
 export class WebsocketService{
     subject = webSocket("wss://9ycacmaxnh.execute-api.us-east-1.amazonaws.com/gc_specific/")
-    user: string = "";
-    signedIn: boolean = false;
+    user: string = "Shreyas";
+    signedIn: boolean = true;
     signingUp: boolean = false;
 }
